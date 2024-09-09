3dde4175eee6e1e159cabe15de2e8e47d4cdf3fa
Paul Reiners <reine097@umn.edu>
2024-09-09 14:02:00 -0500

    Enhanced histograms.

--- img/saliency_maps/create_map.py
+++ img/saliency_maps/create_map.py
@@ -2,6 +2,7 @@ import numpy as np
 import nibabel as nib
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
 
 
 df = pd.read_csv('doc/output.csv')
@@ -15,5 +16,8 @@ for index, row in df.iterrows():
     data = img.get_fdata()
     scaled_data = np.divide(np.multiply(256, data), 2072.0)
     plt.hist(data.flatten(), bins='auto')  
-    plt.title(file)
+    image_file = file.split('/')[-1]
+    image_file = image_file[:-7]
+    plt.title(image_file)
     plt.show()
+    plt.savefig(os.path.join('img/histograms', f'{image_file}.png'))
